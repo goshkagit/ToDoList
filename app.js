b337b24a03f74a715b77ce4706a7d904201e4f4c
Goshka <goshkacuty@gmail.com>
2019-05-29 00:04:33 +0300

    simple validations + full working fetchings

--- app.js
+++ app.js
@@ -4,9 +4,20 @@ const dotenv = require('dotenv');
 
 const bodyParser = require('body-parser');
 
+const joi = require('joi');
+
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use((err , req , res , next)=>{
+    res.status(err.status).json({
+
+       error: {
+           message :err.message
+       }
+    });
+});
 
 app.set('view engine' , 'ejs');
 
@@ -15,6 +26,11 @@ const path = require('path');
 const db = require('./data');
 
 const collection = "todo";
+
+
+const schema = joi.object().keys({
+    todo: joi.string().required()
+});
 
 
 db.connect((err)=>{
@@ -48,45 +64,72 @@ app.get('/' , (res , req) =>{
 });
 
 
-app.put('/:id' , (req , res)=>{
-
+app.put('/:id' , (req , res , next)=> {
     const taskId = req.params.id;
     const userInput = req.body;
 
-    db.getDB().collection(collection).findOneAndUpdate({_id : db.getPrimaryKey(taskId)} ,
-        {$set : {todo: userInput.todo}},{returnOriginal : false},
-        (err , result)=>{
-            if(err)
+
+    joi.validate(userInput, schema, (err, result) => {
+        if (err) {
+            const error = new Error("Invalid Input");
+            error.status = 400;
+            next(error);
+        }
+        else {
+            db.getDB().collection(collection).findOneAndUpdate({_id: db.getPrimaryKey(taskId)},
+                {$set: {todo: userInput.todo}}, {returnOriginal: false},
+                (err, result) => {
+                    if (err)
+                        console.log(err);
+                    else {
+                        res.json(result);
+                    }
+                });
+        }
+    });
+});
+
+    app.post('/', (req, res, next) => {
+
+        const userInput = req.body;
+
+        joi.validate(userInput, schema, (err, result) => {
+            if (err) {
+                const error = new Error("Invalid Input");
+                error.status = 400;
+                next(error);
+            }
+            else {
+
+                db.getDB().collection(collection).insertOne(userInput, (err, result) => {
+                    if (err) {
+                        const error = new Error("Failed to save");
+                        error.status = 400;
+                        next(error);
+                    }
+                    else
+                        res.json({result: result, document: result.ops[0], msg: "Successfully inserted!", error: null});
+                });
+            }
+        });
+
+
+    });
+
+    app.delete('/:id', (req, res) => {
+
+        const taskID = req.params.id;
+
+        db.getDB().collection(collection).findOneAndDelete({_id: db.getPrimaryKey(taskID)}, (err, result) => {
+            if (err)
                 console.log(err);
-            else{
+            else {
                 res.json(result);
             }
         });
-});
 
-app.post('/' , (req , res)=>{
-    const userInput = req.body;
-    db.getDB().collection(collection).insertOne(userInput,(err , result)=>{
-        if(err)
-            console.log(err);
-        else{
-            res.json({result: result, document: result.ops[0]})
-        }
-    })
-});
-
-app.delete('/:id' , (req , res)=>{
-
-    const taskID = req.params.id;
-
-    db.getDB().collection(collection).findOneAndDelete({_id : db.getPrimaryKey(taskID)} , (err, result) =>{
-        if(err)
-            console.log(err);
-        else{
-            res.json(result);
-        }
     });
 
-});
+
 
 
