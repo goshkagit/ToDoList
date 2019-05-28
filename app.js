9c5fd7fe089f900a0c48a9e44b010914d35f0436
Goshka <goshkacuty@gmail.com>
2019-05-28 20:40:28 +0300

    Crud operations

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const db = require('./data');
 
 const collection = "todo";
 
+
 db.connect((err)=>{
     if(err) {
         console.log('Unable to connect to database. Connection error occurred');
@@ -31,10 +32,11 @@ db.connect((err)=>{
 
 
 app.get('/' , (res , req) =>{
+
    req.render('main')
 });
 
-app.get('/getAll' , (res , req) =>{
+ app.get('/getAll' , (res , req) =>{
     db.getDB().collection(collection).find({}).toArray((err , documents)=> {
         if(err)
             console.log(err);
@@ -44,3 +46,47 @@ app.get('/getAll' , (res , req) =>{
         }
     })
 });
+
+
+app.put('/:id' , (req , res)=>{
+
+    const taskId = req.params.id;
+    const userInput = req.body;
+
+    db.getDB().collection(collection).findOneAndUpdate({_id : db.getPrimaryKey(taskId)} ,
+        {$set : {todo: userInput.todo}},{returnOriginal : false},
+        (err , result)=>{
+            if(err)
+                console.log(err);
+            else{
+                res.json(result);
+            }
+        });
+});
+
+app.post('/' , (req , res)=>{
+    const userInput = req.body;
+    db.getDB().collection(collection).insertOne(userInput,(err , result)=>{
+        if(err)
+            console.log(err);
+        else{
+            res.json({result: result, document: result.ops[0]})
+        }
+    })
+});
+
+app.delete('/:id' , (req , res)=>{
+
+    const taskID = req.params.id;
+
+    db.getDB().collection(collection).findOneAndDelete({_id : db.getPrimaryKey(taskID)} , (err, result) =>{
+        if(err)
+            console.log(err);
+        else{
+            res.json(result);
+        }
+    });
+
+});
+
+
